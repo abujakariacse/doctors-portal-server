e908c48ae3a09c6c71213d6039a9d2b60452d768
abujakariacse <abujakaria316@gmail.com>
2022-05-31 18:15:31 +0600

    services api done

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.b8yrs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+
+    try {
+        await client.connect()
+        const serviceCollection = client.db('doctors-portal').collection('service');
+
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir)
+
+
+app.listen(port, () => {
+    console.log("server is runnning on port", port)
+})
