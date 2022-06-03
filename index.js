c69cf869a7219e06e0b1048518e03cfcb1b9db90
abujakariacse <abujakaria316@gmail.com>
2022-06-04 04:04:43 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
@@ -10,16 +11,45 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.b8yrs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Foridden' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 async function run() {
 
     try {
         await client.connect()
         const serviceCollection = client.db('doctors-portal').collection('service');
         const bookingCollection = client.db('doctors-portal').collection('bookings');
+        const userCollection = client.db('doctors-portal').collection('users');
 
         // To check server is running or not on browser
         app.get('/', (req, res) => {
             res.send('Server is running.....')
+        });
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '14d' });
+            res.send({ result, token });
         })
 
         // Get all services
@@ -67,6 +97,26 @@ async function run() {
                 service.slots = available;
             })
             res.send(services)
+        })
+
+        // Get all appointment of specific users
+        app.get('/myappointments', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            const query = { patientEmail: email };
+            const result = await bookingCollection.find(query).toArray();
+            if (decodedEmail === email) {
+                return res.send(result);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden' })
+            }
+        })
+
+        // Get All User
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
         })
 
 
